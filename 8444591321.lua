dd5b58b870880cc25cfee8658bd339cd9738d866
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-01-26 15:08:31 +0200

    Update 8444591321.lua

--- 8444591321.lua
+++ 8444591321.lua
@@ -1,12 +1,14 @@
+local httprequest = (http and http.request or http_request or fluxus and fluxus.request or request or function() end)
 local function vapeGithubRequest(scripturl)
 	if not isfile("vape/"..scripturl) then
-		local suc, res = pcall(function() return requests("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/".Body..readfile("vape/commithash.txt").."/"..scripturl, true) end)
+		local suc, res = pcall(function() return httprequest({Url = "https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/"..readfile("vape/commithash.txt").."/"..scripturl}).Body end)
 		if not suc or res == "404: Not Found" then return nil end
 		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
 		writefile("vape/"..scripturl, res)
 	end
 	return readfile("vape/"..scripturl)
 end
+
 
 shared.CustomSaveVape = 6872274481
 if isfile("vape/CustomModules/6872274481.lua") then
